4ef35d439a42a9daf4461ebbd7702c3fa6742e0f
rodolfoperatello <rodolfoperatello@hotmail.com>
2025-12-17 22:18:10 -0300

    feat - New solution for exercise 3

--- src/main/java/br/com/bmtptecnologia/algorithm/exericises/exercise3.java
+++ src/main/java/br/com/bmtptecnologia/algorithm/exericises/exercise3.java
@@ -7,7 +7,7 @@ import br.com.bmtptecnologia.algorithm.AlgorithmApplication;
 public class exercise3 {
    // Reverse string
 
-   public static String execute(String input) {
+   public static String firstSolution(String input) {
       if (input.length() == 1) {
          return input;
       }
@@ -23,19 +23,39 @@ public class exercise3 {
       return stringBuilder.toString();
    }
 
+   public static String secondSolution(String input) {
+      if (input.length() == 1) {
+         return input;
+      }
+
+      StringBuilder stringBuilder = new StringBuilder();
+
+      for (int i = input.length() - 1; i >= 0; i--) {
+         stringBuilder.append(input.charAt(i));
+      }
+
+      return stringBuilder.toString();
+   }
+
    public static void main(String[] args) {
       SpringApplication.run(AlgorithmApplication.class, args);
 
       String firstInput = "Hello";
 
-      String firstResult = execute(firstInput);
+      String firstResult = firstSolution(firstInput);
 
       System.out.println("Reversed string for " + firstInput + " is " + firstResult);
 
       String secondInput = "Reverse String in Java";
 
-      String secondResult = execute(secondInput);
+      String secondResult = firstSolution(secondInput);
 
       System.out.println("Reversed string for " + firstInput + " is " + secondResult);
+
+      String thirdInput = "Trying a new solution";
+
+      String thirdResult = secondSolution(thirdInput);
+
+      System.out.println("Reversed string for " + thirdInput + " in the second reverse method is " + thirdResult);
    }
 }
